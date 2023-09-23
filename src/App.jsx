9e1298d36613ec6f2c520e7dd696fed9cdde5a02
Eddie344 <e.bazhenov@perenio.com>
2023-09-23 11:24:24 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <>
+      <Cart></Cart>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+    </>
+  );
+}
+
+export default App;
